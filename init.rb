8fd19127a443c0bc683f9271355a20a0ed02f904
Anatoliy Palash <anatoly.palash@gmail.com>
2016-01-06 17:26:47 +0200

    ruby tutorial

---
+++ init.rb
@@ -0,0 +1,20 @@
+require_relative "cart"
+require_relative "item"
+require_relative "virtual_item"
+require_relative "real_item"
+
+item1 = VirtualItem.new({:price => 10, :weight => 100, :name => "Car"})
+item2 = RealItem.new({ :weight => 100, :name => "Car"})
+
+cart = Cart.new
+cart.add_item item1
+cart.add_item item2
+
+p cart.items
+
+cart.delete_invalid_items
+
+p cart.items
+
+p item1.respond_to?(:weight)
+p item2.respond_to?(:weight)
---
+++ item.rb
@@ -0,0 +1,17 @@
+class Item
+
+	def initialize(options={})
+		@price = options[:price]
+		@weight = options[:weight]
+		@name = options[:name]
+	end
+
+	attr_reader :price, :name
+	attr_writer :price
+
+	def info
+		yield(price)
+		yield(name)
+	end
+
+end
